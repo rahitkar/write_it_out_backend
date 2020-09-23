78c278aaf22d58ac21042d4830d71134f640b4b1
rahit_kar <rahit@noreplay.com>
2020-09-24 00:46:55 +0530

    refactored tests

--- test/appTest.js
+++ test/appTest.js
@@ -1,37 +1,18 @@
 const request = require('supertest');
 const { app } = require('../src/app');
 
-describe('/api/user/fetchPoemsData', () => {
-  it('should redirect to localhost:3000 if no session id is present', (done) => {
+describe('/api/user/* ', () => {
+  it('any request of "/api/user/" should redirect to localhost:3000 if no session is present', (done) => {
     const sessions = {};
     sessions.createSession = () => '123';
     sessions.getSession = () => undefined;
     app.locals.sessions = sessions;
-    request(app).get('/api/user/fetchPoemsData').expect(302, done);
-  });
-
-  it('should get poemsData', (done) => {
-    const db = {};
-    db.getPoemsData = () => {
-      return new Promise((resolve, rej) => {
-        resolve('poemData');
-      });
-    };
-
-    const sessions = {};
-    sessions.createSession = () => '123';
-    sessions.getSession = () => '12345';
-    app.locals.sessions = sessions;
-    app.locals.db = db;
-    request(app)
-      .get('/api/user/fetchPoemsData')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
+    request(app).get('/api/user/wxyz').expect(302, done);
   });
 });
 
-describe('/api/user/addPoemData', () => {
-  it('should add poemData', (done) => {
+describe('any request of /api/user/ with session id present', () => {
+  beforeEach(() => {
     const db = {};
     db.getPoemsData = () => {
       return new Promise((resolve, rej) => {
@@ -51,27 +32,6 @@ describe('/api/user/addPoemData', () => {
       });
     };
 
-    const sessions = {};
-    sessions.createSession = () => '123';
-    sessions.getSession = () => '12345';
-    app.locals.sessions = sessions;
-    app.locals.db = db;
-    request(app)
-      .post('/api/user/addPoemData')
-      .send({ data: 'poemData' })
-      .expect(200, done);
-  });
-});
-
-describe('/api/user/getUserDetails/:userId', () => {
-  it('should get userDetails for given userId', (done) => {
-    const db = {};
-    db.getPoemsData = () => {
-      return new Promise((resolve, rej) => {
-        resolve([]);
-      });
-    };
-
     db.getUser = (id) => {
       return new Promise((resolve, rej) => {
         if (id) {
@@ -85,6 +45,29 @@ describe('/api/user/getUserDetails/:userId', () => {
     sessions.getSession = () => '12345';
     app.locals.sessions = sessions;
     app.locals.db = db;
-    request(app).get('/api/user/getUserDetails/1').expect(200, done);
+  });
+
+  describe('/api/user/fetchPoemsData', () => {
+    it('should get poemsData', (done) => {
+      request(app)
+        .get('/api/user/fetchPoemsData')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+  });
+
+  describe('/api/user/addPoemData', () => {
+    it('should add poemData', (done) => {
+      request(app)
+        .post('/api/user/addPoemData')
+        .send({ data: 'poemData' })
+        .expect(200, done);
+    });
+  });
+
+  describe('/api/user/getUserDetails/:userId', () => {
+    it('should get userDetails for given userId', (done) => {
+      request(app).get('/api/user/getUserDetails/1').expect(200, done);
+    });
   });
 });
