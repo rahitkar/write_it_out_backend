8eb9691bd6b994e8c24aabea6b708b9b7d4dd59c
rahit_kar <rahit@noreplay.com>
2020-09-29 17:52:24 +0530

    added absolute path for serving build dir

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/../build`));
 app.use((req, res, next) => {
   console.log(req.url);
   next();
